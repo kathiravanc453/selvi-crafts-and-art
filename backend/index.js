be4d6d9f1b76f3d41d8bad7e275af7fab2744133
kathiravanc453 <kathiravanc453@gmail.com>
2026-08-22 16:27:14 +0530

    Make backend module self-contained with backend/database.js to resolve ERR_MODULE_NOT_FOUND on Render

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
-import { query, queryOne, run } from '../database/database.js';
+import { query, queryOne, run } from './database.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
